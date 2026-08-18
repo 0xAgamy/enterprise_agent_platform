bade9195fd321ebbcfe00975fc39ac209fc009f6
0xAgamy <mohamedelagamy516@gmail.com>
2026-08-18 17:39:21 +0300

    prompt versioning

---
+++ helpers/prompt_management.py
@@ -0,0 +1,8 @@
+import yaml
+from jinja2 import Template
+
+def prompt_template_config(yaml_file, prompt_key):
+    with open(yaml_file,'r') as file:
+        config= yaml.safe_load(file)
+    template_content= config["prompts"][prompt_key]
+    return Template(template_content)
