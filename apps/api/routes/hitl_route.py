d467d56717fda2e721f207bcf504dcb84e751b08
0xAgamy <mohamedelagamy516@gmail.com>
2026-09-03 11:58:37 +0300

    update HITL api with the new depends

--- apps/api/routes/hitl_route.py
+++ apps/api/routes/hitl_route.py
@@ -1,15 +1,21 @@
-from fastapi import Request , APIRouter
+from fastapi import Request , APIRouter, Depends
 from fastapi.responses import StreamingResponse
 from agents.graph import run_agent_stream_wrapper
 from ..models.schema import HitlRequest
+from apps.api.dependencies.dependencies import AppDependencies,get_app_dependencies
 
 
 
 hitl_router= APIRouter()
 
 @hitl_router.post("/")
-def hitl(request: Request,payload: HitlRequest)->StreamingResponse:
+def hitl(request: Request,payload: HitlRequest, deps:AppDependencies= Depends(get_app_dependencies))->StreamingResponse:
+    workflow= deps.graph
+    mcp_tools_description= deps.mcp_tools
     return StreamingResponse(
-        run_agent_stream_wrapper(payload.approved,payload.thread_id,"hitl"),
+        run_agent_stream_wrapper(workflow,
+                                mcp_tools_description,
+                                payload.approved,
+                                payload.thread_id,"hitl"),
         media_type="text/event-stream"
         )
